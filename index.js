ffe9fb244d947930383c987f1c5459188e7b1065
alfredugl <107470305+alfredugl@users.noreply.github.com>
2022-08-18 19:39:36 +0100

    nothing much

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ temp = toCelcius(temp);
 console.log(temp);
 
 function toCelcius(temp){
-    return (temp - 32) * (5/9);
+    return (temp - 3) * (5/9);
 }
 function toFahrenheit(temp){
     return temp * 9/5 + 32;
