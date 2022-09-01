492f4bea4a97f86ee096d0c0c873d894575e7cd0
alfredugl <107470305+alfredugl@users.noreply.github.com>
2022-09-01 21:50:21 +0100

    chai

--- indexx.js
+++ indexx.js
@@ -26,4 +26,31 @@ function texttospeech(text) {
     speechSynthesis.speak(utterance)
 }
 
-
+speechbtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (textarea.value != '') {
+        if (!synth.speaking) {
+            texttospeech(textarea.value)
+        }
+        if (textarea.value.length > 70) {
+            if(isSpeaking) {
+                synth.resume()
+                isSpeaking = false
+                speechbtn.innerHTML = 'Pause Speech'
+            } else {
+                synth.pause()
+                isSpeaking = true
+                speechbtn.innerHTML = 'Resume Speech'
+            }
+            setInterval(() => {
+               if (!synth.speaking && !isSpeaking){
+                   isSpeaking = true
+                   speechbtn.innerHTML = 'Convert to Speech'
+               } 
+            })
+        }else {
+            speechbtn.innerHTML = 'Convert To Speech'
+        }
+    }
+})
+voicespeech()
